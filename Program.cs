7ddaf9954671acd480355ed8ef39f336d6b59b3d
João Pedro de Bem <jpdebem@gmail.com>
2023-12-19 13:49:11 -0300

    Metodos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿namespace Archivum;
+﻿using Archivum.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Archivum;
+
+public class Program
+{
+    private const string CONNECTION_STRING = @"Server=localhost,1433;
+                                               Database=balta;
+                                               User ID=sa;
+                                               Password=D3vInD0ck3r$@#$;
+                                               Trusted_Connection=False; 
+                                               TrustServerCertificate=True;";
+    public static void Main(string[] args)
+    {
+
+    }
+    public static void ReadUsers()
+    {
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var users = connection.GetAll<User>();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+    }
+
+}
